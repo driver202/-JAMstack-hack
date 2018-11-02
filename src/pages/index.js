300554d6c31bd65460316e81eb96816c0694c7e3
Author Name <email@address.com>
2018-11-02 21:21:43 +0400

    first commit
     Author:    Tigran Sahakyan <driver202@gmail.com>
     Date:      Fri Nov 2 21:21:43 2018 +0400

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p><a href="https://www.behance.net/driver202de98">Newman</a> is here! Joining CMintS team as a UX/UI Designer! Check out some of my tunes meanwhile!</p>
+    <iframe width="100%" height="120" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&feed=%2Ftigrannewman%2F" frameborder="0" ></iframe>
+  </Layout>
+)
+
+export default IndexPage
